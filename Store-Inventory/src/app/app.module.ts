2d917ddd01022021f0858bdb178515ecf4c200ee
JhonFaverM <91922585+JhonFaverM@users.noreply.github.com>
2021-12-27 23:07:48 -0500

    Actualizacion app

--- Store-Inventory/src/app/app.module.ts
+++ Store-Inventory/src/app/app.module.ts
@@ -2,29 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
-//angular material
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { SharedModule } from './components/shared/shared.module';
+
+
+//import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+//componentes
 import { LoginComponent } from './components/login/login.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
-//import { DirectionsBikeIcon } from '@mui/icons-material/DirectionsBike';
-
+import { ProductsComponent } from './components/products/products.component';
 
 
 
@@ -32,27 +21,14 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
   declarations: [
     AppComponent,
     ProductsComponent,
-    SidenavComponent,
     LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule,
-    MatInputModule
-  
-    //DirectionsBikeIcon
-
+    SharedModule
+    //MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Store-Inventory/src/app/components/shared/shared.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//modulos
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+//Angular material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
+  ]
+})
+export class SharedModule { }
